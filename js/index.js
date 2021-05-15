4443042fdfc2fabf49b3fd3842c5a578040eec97
TansanSui-H2CO3 <tansanwater.h2co3@gmail.com>
2021-05-16 02:56:55 +0900

    style: index.html

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 let map = L.map('map').setView([35.30, 138.30] , 5);
-L.tileLayer('http://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-    attribution: '? OpenStreetMap contributors',
-    maxZoom: 18,
-    tileSize: 512,
-    zoomOffset: -1,
+L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy Esri &mdash Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    maxZoom: 18
 }).addTo(map);
